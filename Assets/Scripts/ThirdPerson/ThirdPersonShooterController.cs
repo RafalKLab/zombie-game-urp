a92cc1c337e533922767f060249f9cb1cab2c6d0
Rafal <kadzewiczrafal@gmail.com>
2026-02-11 21:19:41 +0300

    Add cowboy

--- Assets/Scripts/ThirdPerson/ThirdPersonShooterController.cs
+++ Assets/Scripts/ThirdPerson/ThirdPersonShooterController.cs
@@ -18,28 +18,32 @@ public class ThirdPersonShooterController : MonoBehaviour
 
     private StarterAssetsInputs starterAssetsInputs;
     private ThirdPersonController thirdPersonController;
+    private Animator animator;
 
     private void Awake()
     {
         starterAssetsInputs = GetComponent<StarterAssetsInputs>();
         thirdPersonController = GetComponent<ThirdPersonController>();
+        animator = GetComponent<Animator>();
 
         brain.DefaultBlend.Time = 0.1f;
     }
 
     private void Update()
     {
-        Vector3 mouseWorldPosition = Vector3.zero;
-
-        Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
-        Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
-        if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, aimLayerMask))
-        {
-            mouseWorldPosition = raycastHit.point;
-        }
-
         if (starterAssetsInputs.aim)
         {
+            animator.SetLayerWeight(1, Mathf.Lerp(animator.GetLayerWeight(1), 1f, Time.deltaTime * 10f));
+
+            Vector3 mouseWorldPosition = Vector3.zero;
+
+            Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
+            Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
+            if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, aimLayerMask))
+            {
+                mouseWorldPosition = raycastHit.point;
+            }
+
             aimVirtualCamera.gameObject.SetActive(true);
             thirdPersonController.Sensitivity = aimSensitivity;
             thirdPersonController.SetRotateOnMove(false);
@@ -52,17 +56,19 @@ public class ThirdPersonShooterController : MonoBehaviour
 
         } else
         {
+            animator.SetLayerWeight(1, Mathf.Lerp(animator.GetLayerWeight(1), 0f, Time.deltaTime * 10f));
+
             aimVirtualCamera.gameObject.SetActive(false);
             thirdPersonController.Sensitivity = normalSensitivity;
             thirdPersonController.SetRotateOnMove(true);
         }
 
-        if (starterAssetsInputs.shoot)
-        {
-            Vector3 aimDir = (mouseWorldPosition - spawnBulletPosition.position).normalized;
+        //if (starterAssetsInputs.shoot)
+        //{
+        //    Vector3 aimDir = (mouseWorldPosition - spawnBulletPosition.position).normalized;
 
-            Instantiate(bulletPrefab, spawnBulletPosition.position, Quaternion.LookRotation(aimDir, Vector3.up));
-            starterAssetsInputs.shoot = false;
-        }
+        //    Instantiate(bulletPrefab, spawnBulletPosition.position, Quaternion.LookRotation(aimDir, Vector3.up));
+        //    starterAssetsInputs.shoot = false;
+        //}
     }
 }
